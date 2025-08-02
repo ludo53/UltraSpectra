2c7f3a74eaeee6fb642377f423334b02a8628485
Ludo <97764715+ludo53@users.noreply.github.com>
2025-08-02 20:12:31 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/ultraspectra/main.go
@@ -0,0 +1,23 @@
+// cmd/ultraspectra/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"ultraspectra/internal/ultraspectra"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := ultraspectra.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
